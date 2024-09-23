b52be2eaf5252b21a87f9f0676c1fce79683e1b9
Deivid <deividvelasquez122@gmail.com>
2024-09-23 17:16:18 -0500

    feat: :bug: Se corrigio link de api traducida al ingles

--- Dia3/Restaurante/Restaurante/src/main/java/com/example/Restaurante/controller/MesaController.java
+++ Dia3/Restaurante/Restaurante/src/main/java/com/example/Restaurante/controller/MesaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/mesas")
+@RequestMapping("/api/table")
 public class MesaController {
 
     @Autowired
